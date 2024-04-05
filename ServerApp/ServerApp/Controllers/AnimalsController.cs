85e1c9a291a74c1761d925f65e1392c00bec4499
Veronika <veronika.novosad4@gmail.com>
2024-04-05 18:07:06 +0200

    Initial app

---
+++ ServerApp/ServerApp/Controllers/AnimalsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServerApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnimalsController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok();
+    }
+}
